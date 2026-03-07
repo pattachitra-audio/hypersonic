0d1cdab42214ae82191cfd1684cf250f920bb544
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-08 01:28:17 +0530

    Added logging requests for tRPC api

--- src/app/api/tRPC/[tRPC]/route.ts
+++ src/app/api/tRPC/[tRPC]/route.ts
@@ -1,12 +1,15 @@
 import { router } from "@/server/router";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-const handler = (req: Request) =>
-    fetchRequestHandler({
+const handler = (req: Request) => {
+    console.log("Request URL:", req.url);
+
+    return fetchRequestHandler({
         endpoint: "/api/tRPC",
         req,
         router,
         createContext: () => ({}),
     });
+};
 
 export { handler as GET, handler as POST };
