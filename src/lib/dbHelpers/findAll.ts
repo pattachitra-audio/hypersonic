7fa01a11227c8db20c41d2246e43cc581805b6d2
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-14 09:30:30 +0530

    Added DB helper functions

---
+++ src/lib/dbHelpers/findAll.ts
@@ -0,0 +1,20 @@
+import NoThrow from "@/utils/NoThrow";
+import { Collection, Document, MongoError } from "mongodb";
+
+export default async function findAll<T extends Document>(collection: Collection<T>) {
+    try {
+        const result = await collection.find().toArray();
+        console.log("result:", result);
+        return NoThrow.ok(result);
+    } catch (error) {
+        if (error instanceof MongoError) {
+            return NoThrow.err(error);
+        }
+
+        if (error instanceof Error) {
+            return NoThrow.err(error);
+        }
+
+        return NoThrow.err(new Error("Unknown error"));
+    }
+}
---
+++ src/lib/dbHelpers/insertOne.ts
@@ -0,0 +1,25 @@
+import NoThrow from "@/utils/NoThrow";
+import { Collection, Document, MongoError, OptionalUnlessRequiredId } from "mongodb";
+
+export async function insertOne<T extends Document>(document: OptionalUnlessRequiredId<T>, collection: Collection<T>) {
+    try {
+        const result = await collection.insertOne(document);
+
+        /* if (result.acknowledged) {
+                    return NoThrow.ok(result.insertedId);
+                } */
+
+        return NoThrow.ok(result);
+        // return NoThrow.err(new Error("MongoDB 'insertOne' err"));
+    } catch (error) {
+        if (error instanceof MongoError) {
+            return NoThrow.err(error);
+        }
+
+        if (error instanceof Error) {
+            return NoThrow.err(error);
+        }
+
+        return NoThrow.err(new Error("Unknown error"));
+    }
+}
