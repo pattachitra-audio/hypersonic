6de7e34e378690492d3d46dff01e8062963b2186
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-15 13:41:46 +0530

    Added common headers to mimic requests sent by browsers

---
+++ src/requestHeaders.ts
@@ -0,0 +1,9 @@
+export const requestHeaders = {
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
+    "Sec-Ch-Ua": '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+};
