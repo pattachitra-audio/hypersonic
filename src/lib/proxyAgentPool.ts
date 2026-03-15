b6731fb315cf7943331828e97291c28a943b6306
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-15 13:41:18 +0530

    Added proxyAgentPool to reuse proxy connections

---
+++ src/lib/proxyAgentPool.ts
@@ -0,0 +1,45 @@
+import { ProxyURLType } from "@/types/proxyURL";
+import { ProxyAgent } from "undici";
+
+class ProxyAgentPool {
+    private proxyURLToAgent: Map<string, ProxyAgent>;
+
+    constructor() {
+        this.proxyURLToAgent = new Map<string, ProxyAgent>();
+    }
+
+    get(proxyURL: ProxyURLType) {
+        const proxyAgent = this.proxyURLToAgent.get(proxyURL);
+
+        if (proxyAgent) {
+            return proxyAgent;
+        }
+
+        const newProxyAgent = new ProxyAgent({
+            uri: proxyURL,
+            connections: 20,
+            keepAliveTimeout: 172_800_000 /* 2 days */,
+        });
+        this.proxyURLToAgent.set(proxyURL, newProxyAgent);
+
+        return newProxyAgent;
+    }
+
+    async close(proxyURL: ProxyURLType) {
+        const proxyAgent = this.proxyURLToAgent.get(proxyURL);
+
+        if (!proxyAgent) {
+            return;
+        }
+
+        await proxyAgent.close();
+        this.proxyURLToAgent.delete(proxyURL);
+    }
+
+    async closeAll() {
+        await Promise.all([...this.proxyURLToAgent.values()].map((proxyAgent) => proxyAgent.close()));
+        this.proxyURLToAgent.clear();
+    }
+}
+
+export const proxyAgentPool = new ProxyAgentPool();
