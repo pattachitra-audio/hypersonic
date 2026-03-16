42f529081e2b681981f71cc87a3ac13244a8010b
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-16 23:34:45 +0530

    Added utils

---
+++ src/utils/decodeElevenLabsFirebaseJWTPayload.ts
@@ -0,0 +1,63 @@
+import z from "zod";
+import { NoThrow } from "./NoThrow";
+
+const ElevenLabsFirebaseJWTSchema = z
+    .object({
+        workspace_id: z.string(),
+        workspace_user_id: z.string(),
+        user_id: z.string(),
+        email: z.string(),
+        email_verified: z.boolean(),
+    })
+    .transform(
+        ({
+            workspace_id: workspaceID,
+            workspace_user_id: workspaceUserID,
+            user_id: userID,
+            email_verified: emailVerified,
+            ...rest
+        }) => ({
+            workspaceID,
+            workspaceUserID,
+            userID,
+            emailVerified,
+            ...rest,
+        }),
+    );
+
+export async function decodeElevenLabsFirebaseJWTPayload(jwt: string) {
+    const parts = jwt.split(".");
+
+    if (parts.length !== 3) {
+        return NoThrow.err(
+            new Error(
+                `Invalid jwt format; Expected '<header>.<payload>.<signature>', where 'header', 'payload', and 'signature' are base64 strings`,
+            ),
+        );
+    }
+
+    const payloadBase64 = parts[1];
+    let payloadString: string;
+
+    try {
+        payloadString = atob(payloadBase64);
+    } catch (error) {
+        return NoThrow.err(new Error("Error while decoding base64", { cause: error }));
+    }
+
+    let payloadObject: unknown;
+
+    try {
+        payloadObject = JSON.parse(payloadString);
+    } catch (error) {
+        return NoThrow.err(new Error("Error while parsing (JSON) payload string", { cause: error }));
+    }
+
+    const payloadResult = await ElevenLabsFirebaseJWTSchema.safeParseAsync(payloadObject);
+
+    if (!payloadResult.success) {
+        return NoThrow.err(payloadResult.error);
+    }
+
+    return NoThrow.ok(payloadResult.data);
+}
---
+++ src/utils/parseProxyURL.ts
@@ -0,0 +1,14 @@
+import { ProxyURLType } from "@/types/proxyURL";
+import { NoThrow } from "./NoThrow";
+
+export function parseProxyURL(proxyURL: string) {
+    const pattern = /^http:\/\/.+:.+@.+:.+$/;
+
+    if (!pattern.test(proxyURL)) {
+        return NoThrow.err(
+            new Error(`Invalid proxy url format '${proxyURL}'; Expected 'http://<username>:<password>@<host>:<port>`),
+        );
+    }
+
+    return NoThrow.ok(proxyURL as ProxyURLType);
+}
