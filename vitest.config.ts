f3ab6ed116b33a8e2c9797add38c55bdea71886f
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-17 00:13:52 +0530

    Added globalSetup, dotEnv; Fixed tsconfig path to tsconfig.app.json

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,17 @@
 import path from "node:path";
 import { defineConfig } from "vitest/config";
-import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.test" });
+import dotEnv from "dotenv";
+dotEnv.config({ path: ".env.test", override: true });
 
 export default defineConfig({
     test: {
         alias: {
             "@": path.resolve(__dirname, "src"),
         },
+        globalSetup: "test/globalSetup",
         typecheck: {
-            tsconfig: "./tsconfig.test.json",
+            tsconfig: "tsconfig.test.json",
         },
     },
 });
