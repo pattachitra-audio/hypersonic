601553dbc5ac3c9cafb8a3303087a35a055d1848
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-16 23:46:02 +0530

    Updated next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    typescript: {
+        tsconfigPath: "tsconfig.app.json",
+    },
+    turbopack: {},
 };
 
 export default nextConfig;
