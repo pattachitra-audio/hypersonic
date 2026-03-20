d582d50f74ab309346d3404d8f1e96bfe6cda2e5
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-20 15:38:55 +0530

    updated globalSetup

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -2,13 +2,12 @@ import type { TestProject } from "vitest/node";
 import { setupFirebaseAuth } from "./helpers/setupFirebaseAuth";
 
 export async function setup(testProject: TestProject) {
-    const result = await setupFirebaseAuth();
+    const firebaseAuthResult = await setupFirebaseAuth();
 
-    if (result.isErr()) {
-        throw result.error;
+    if (firebaseAuthResult.isErr()) {
+        throw firebaseAuthResult.error;
     }
 
     console.log("Setup firebase auth ✔");
-
-    testProject.provide("firebaseAuth", result.value);
+    testProject.provide("firebaseAuth", firebaseAuthResult.value);
 }
