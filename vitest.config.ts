73bf980c6c23d1b1f546b8f1cea97955c608939e
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-20 15:43:31 +0530

    Increased test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
                         "@": "src",
                         test: "test",
                     },
+                    testTimeout: 120_000,
 
                     /* alias: {
                         "@/": "src/",
