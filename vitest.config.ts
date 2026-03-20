b7feeb334c9d570c1d6c5b5835fa98641f2b50df
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-20 15:39:10 +0530

    updated test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-import path from "node:path";
 import { defineConfig } from "vitest/config";
 import dotEnv from "dotenv";
 
@@ -7,9 +6,12 @@ if (!process.env.GITHUB_ACTIONS) {
 }
 
 export default defineConfig({
+    // plugins: [tsconfigPaths({ projects: ["./tsconfig.test.json"] })],
+
     test: {
+        testTimeout: 30000,
         alias: {
-            "@": path.resolve(__dirname, "src"),
+            "@": "src",
         },
         globalSetup: "test/globalSetup",
         typecheck: {
@@ -21,5 +23,25 @@ export default defineConfig({
             reportsDirectory: "./coverage",
             include: ["src/**/*.ts"],
         },
+        // include: ["test/**/*.ts"],
+
+        projects: [
+            {
+                test: {
+                    name: "elevenLabsInternalAPI",
+                    include: ["test/services/elevenLabsInternalAPI/**/*test.ts"],
+                    // maxWorkers: 1,
+                    alias: {
+                        "@": "src",
+                        test: "test",
+                    },
+
+                    /* alias: {
+                        "@/": "src/",
+                        // test: path.resolve(__dirname, "test"),
+                    }, */
+                },
+            },
+        ],
     },
 });
