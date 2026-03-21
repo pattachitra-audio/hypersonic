efc9396be26422f33ab29d7fc96b229fc6a6838f
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-21 10:22:46 +0530

    Added NoThrow function 'fromZodResultType'

--- src/utils/NoThrow.ts
+++ src/utils/NoThrow.ts
@@ -1,10 +1,19 @@
 // import { Err, Ok } from "neverthrow";
+import z from "zod";
 import { err, ok } from "neverthrow";
 import type { Result, ResultAsync } from "neverthrow";
 
 export const NoThrow = {
     err,
     ok,
+
+    fromZodResultType<T>(result: z.ZodSafeParseResult<T>) {
+        if (!result.success) {
+            return NoThrow.err(result.error);
+        }
+
+        return NoThrow.ok(result.data);
+    },
 };
 
 export { Result, ResultAsync };
