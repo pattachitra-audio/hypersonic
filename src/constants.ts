06c4d93059744ff8b309cf19d698b3b9d6e17494
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-24 22:01:00 +0530

    ADDED constant for OWNER_ID for now; This will be used as the current userID, until auth is implemented

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
+import { ObjectId } from "mongodb";
+
 export const INVALID_INDEX = 1000000000;
+export const OWNER_ID = ObjectId.createFromHexString("fade-deaf-face-babe-cafe-deed".replaceAll("-", ""));
