ce479f677d5425a2a5392facf93a00d2a5f590cc
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-24 22:22:31 +0530

    Updated prettify, to not always unwrap types to it's corresponding object key-value pairs

--- src/utils/prettify.ts
+++ src/utils/prettify.ts
@@ -1,3 +1,5 @@
-export type Prettify<T extends object> = {
-    [K in keyof T]: T[K] extends object ? Prettify<T[K]> : T[K];
+import { ObjectId } from "mongodb";
+
+export type Prettify<T extends object, Skip = ObjectId> = {
+    [K in keyof T]: T[K] extends object ? (T[K] extends Skip ? T[K] : Prettify<T[K]>) : T[K];
 } & {};
