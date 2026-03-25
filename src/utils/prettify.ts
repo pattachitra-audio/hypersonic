9040568898815e93acabd18c8209993cfe6be1b3
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-03-25 23:10:36 +0530

    Improved prettify.ts

--- src/utils/prettify.ts
+++ src/utils/prettify.ts
@@ -1,5 +1,7 @@
 import { ObjectId } from "mongodb";
 
-export type Prettify<T extends object, Skip = ObjectId> = {
-    [K in keyof T]: T[K] extends object ? (T[K] extends Skip ? T[K] : Prettify<T[K]>) : T[K];
+type GlobalSkip = ObjectId;
+
+export type Prettify<T extends object, Skip = never> = {
+    [K in keyof T]: T[K] extends infer X extends object ? (X extends Skip | GlobalSkip ? X : Prettify<X>) : T[K];
 } & {};
