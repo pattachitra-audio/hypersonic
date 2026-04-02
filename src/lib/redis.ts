5bd68d569d7b83991fcccb29e9c8705a0e5efe48
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-02 06:34:31 +0530

    Simplified redis client

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,14 +2,12 @@ import { ResultAsync } from "neverthrow";
 import { envPromise } from "./env";
 import { createClient } from "redis";
 
-export const redisClientPromise = (function () {
-    return envPromise.andThen((env) => {
-        const redisClient = createClient({
-            url: env.REDIS_URL,
-        });
-
-        return ResultAsync.fromPromise(redisClient.connect(), (error: unknown) => {
-            return new Error("Redis connection error", { cause: error });
-        });
+export const redisClientPromise = envPromise.andThen((env) => {
+    const redisClient = createClient({
+        url: env.REDIS_URL,
     });
-})();
+
+    return ResultAsync.fromPromise(redisClient.connect(), (error: unknown) => {
+        return new Error("Redis connection error", { cause: error });
+    });
+});
