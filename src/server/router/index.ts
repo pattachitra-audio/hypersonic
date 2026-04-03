eb5df9ade358fbf8486b39c8310125ed04f18050
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-03 09:26:16 +0530

    Added prao router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,11 +2,13 @@ import { tRPCRouter } from "@/server/tRPC";
 import { projectRouter } from "./project";
 import { projectsRouter } from "./projects";
 import { elevenLabsAccountWithProxyRouter } from "./elevenLabsAccountWithProxy";
+import { praoRouter } from "./prao";
 
 export const router = tRPCRouter({
     project: projectRouter,
     projects: projectsRouter,
     elevenLabsAccountWithProxy: elevenLabsAccountWithProxyRouter,
+    prao: praoRouter,
 });
 
 export type Router = typeof router;
