85e574c33255e0f3941eb78dac4be2c8f119608c
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-03 09:22:49 +0530

    neverthrow paradigm

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -1,13 +1,6 @@
 import type { TestProject } from "vitest/node";
 import { setupFirebaseAuth } from "./helpers/setupFirebaseAuth";
 
-export async function setup(testProject: TestProject) {
-    const firebaseAuthResult = await setupFirebaseAuth();
-
-    if (firebaseAuthResult.isErr()) {
-        throw firebaseAuthResult.error;
-    }
-
-    console.log("Setup firebase auth ✔");
-    testProject.provide("firebaseAuth", firebaseAuthResult.value);
+export function setup(testProject: TestProject) {
+    return setupFirebaseAuth().map((firebaseAuth) => testProject.provide("firebaseAuth", firebaseAuth));
 }
