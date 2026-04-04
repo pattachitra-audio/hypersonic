76cc644ca5948aa43595854047c5dd323ee801ad
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-04 12:45:34 +0530

    Added session destroy endpoint

---
+++ src/server/router/praoSession/destroy.ts
@@ -0,0 +1,57 @@
+import z from "zod";
+import { tRPCProcedure } from "@/server/tRPC";
+import { ObjectId } from "mongodb";
+import { getErrorMessage } from "@/utils/getErrorMessage";
+import { PRAOSessionRepositoryResultAsync } from "@/repository/PRAOSession";
+import { ElevenLabsAccountWithProxyRepositoryResultAsync } from "@/repository/ElevenLabsAccountWithProxyRepository";
+import { ResultAsync } from "neverthrow";
+import { RedisClientResultAsync } from "@/lib/RedisClient";
+import { TRPCError } from "@trpc/server";
+
+const InputSchema = z.object({
+    sessionID: z
+        .hex()
+        .length(24)
+        .transform((string, context) => {
+            try {
+                return ObjectId.createFromHexString(string);
+            } catch (error) {
+                context.addIssue({
+                    code: "custom",
+                    message: getErrorMessage(error),
+                });
+
+                return z.NEVER;
+            }
+        }),
+});
+
+export const destroyProcedure = tRPCProcedure.input(InputSchema).mutation(async ({ input }) => {
+    const result = await destroy(input.sessionID);
+
+    if (result.isErr()) {
+        throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            cause: result.error,
+            message: getErrorMessage(result.error),
+        });
+    }
+
+    return result.value;
+});
+
+export function destroy(sessionID: ObjectId) {
+    return ResultAsync.combine([
+        PRAOSessionRepositoryResultAsync,
+        ElevenLabsAccountWithProxyRepositoryResultAsync,
+        RedisClientResultAsync,
+    ]).andThen(([PRAOSessionRepositoryResultAsync, ElevenLabsAccountWithProxyRepository, RedisClient]) =>
+        PRAOSessionRepositoryResultAsync.deleteOneByID(sessionID).andThen((session) =>
+            ResultAsync.combine([
+                ElevenLabsAccountWithProxyRepository.unlockMany(session.accountIDs),
+                RedisClient.del(`ElevenLabsCreditsSession@${sessionID}`),
+                RedisClient.del(`ElevenLabsFreeSession@${sessionID}`),
+            ]),
+        ),
+    );
+}
--- src/server/router/praoSession/index.ts
+++ src/server/router/praoSession/index.ts
@@ -1,6 +1,8 @@
 import { tRPCRouter } from "@/server/tRPC";
 import { initProcedure } from "./init";
+import { destroyProcedure } from "./destroy";
 
 export const praoSessionRouter = tRPCRouter({
     init: initProcedure,
+    destroy: destroyProcedure,
 });
