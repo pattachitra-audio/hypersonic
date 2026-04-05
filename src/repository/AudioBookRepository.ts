a95aa1f6b200aaa2db9729fc4a1799931a40d92e
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-05 17:17:08 +0530

    Removed useless fn wrapper

--- src/repository/AudioBookRepository.ts
+++ src/repository/AudioBookRepository.ts
@@ -29,53 +29,51 @@ export type AudioBookSummaryDocumentType = {
     status: "ACTIVE" | "ARCHIVED" | "DELETED";
 };
 
-export const AudioBookRepositoryResultAsync = (function () {
-    return DBClientResultAsync.andThen((dbClient) => {
-        const db = dbClient.db("core");
-        const collection = db.collection<AudioBookDocumentType>("AudioBook");
+export const AudioBookRepositoryResultAsync = DBClientResultAsync.andThen((dbClient) => {
+    const db = dbClient.db("core");
+    const collection = db.collection<AudioBookDocumentType>("AudioBook");
 
-        return ok({
-            findAllSummaries() {
-                return ResultAsync.fromPromise(
-                    collection
-                        .aggregate<AudioBookSummaryDocumentType>([
-                            {
-                                $project: {
-                                    id: "$_id",
-                                    _id: 0,
-                                    name: 1,
-                                    plot: 1,
-                                    genre: 1,
-                                    numCharacters: { $size: "$characters" },
-                                    numEpisodes: { $size: "$episodes" },
-                                    numScenes: { $size: "$scenes" },
-                                    numDialouges: { $size: "$dialogues" },
-                                    createdAt: 1,
-                                    updatedAt: 1,
-                                    lastAccessedAt: 1,
-                                    status: 1,
-                                },
+    return ok({
+        findAllSummaries() {
+            return ResultAsync.fromPromise(
+                collection
+                    .aggregate<AudioBookSummaryDocumentType>([
+                        {
+                            $project: {
+                                id: "$_id",
+                                _id: 0,
+                                name: 1,
+                                plot: 1,
+                                genre: 1,
+                                numCharacters: { $size: "$characters" },
+                                numEpisodes: { $size: "$episodes" },
+                                numScenes: { $size: "$scenes" },
+                                numDialouges: { $size: "$dialogues" },
+                                createdAt: 1,
+                                updatedAt: 1,
+                                lastAccessedAt: 1,
+                                status: 1,
                             },
-                        ])
-                        .toArray(),
-                    handleMongoError,
-                );
-            },
-            findOneByID(id: ObjectId) {
-                return ResultAsync.fromPromise(collection.findOne({ _id: id, status: "ACTIVE" }), handleMongoError);
-            },
+                        },
+                    ])
+                    .toArray(),
+                handleMongoError,
+            );
+        },
+        findOneByID(id: ObjectId) {
+            return ResultAsync.fromPromise(collection.findOne({ _id: id, status: "ACTIVE" }), handleMongoError);
+        },
 
-            insertOne(document: AudioBookDocumentType) {
-                return insertOne(document, collection);
-            },
+        insertOne(document: AudioBookDocumentType) {
+            return insertOne(document, collection);
+        },
 
-            replaceOneByID(id: ObjectId, document: AudioBookDocumentType) {
-                return ResultAsync.fromPromise(collection.replaceOne({ _id: id }, document), handleMongoError);
-            },
+        replaceOneByID(id: ObjectId, document: AudioBookDocumentType) {
+            return ResultAsync.fromPromise(collection.replaceOne({ _id: id }, document), handleMongoError);
+        },
 
-            updateOne(filter: Filter<AudioBookDocumentType>, updateFilter: UpdateFilter<AudioBookDocumentType>) {
-                return ResultAsync.fromPromise(collection.updateOne(filter, updateFilter), handleMongoError);
-            },
-        });
+        updateOne(filter: Filter<AudioBookDocumentType>, updateFilter: UpdateFilter<AudioBookDocumentType>) {
+            return ResultAsync.fromPromise(collection.updateOne(filter, updateFilter), handleMongoError);
+        },
     });
-})();
+});
