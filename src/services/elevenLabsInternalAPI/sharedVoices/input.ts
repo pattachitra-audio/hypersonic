8223de01f706b9350d576e229b9de2d2742e9b6c
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-05 20:03:49 +0530

    Updated input schema

--- src/services/elevenLabsInternalAPI/sharedVoices/input.ts
+++ src/services/elevenLabsInternalAPI/sharedVoices/input.ts
@@ -6,7 +6,7 @@ export const InputSchema = z.object({
     bearerToken: z.string(),
 
     searchQuery: z.string().optional(),
-    sort: z.enum(["trending"]).default("trending"),
+    sort: z.enum(["TRENDING"]).default("TRENDING"),
     pageSize: z.int().min(1).max(100).default(30),
     pageNum: z.int(),
 });
