39aa6ebf418bbbea35a582c654f646ffde4a7ba3
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-05 20:03:03 +0530

    Fixed 'sharedVoices' procedure

--- src/server/router/elevenLabsInternal/sharedVoices.ts
+++ src/server/router/elevenLabsInternal/sharedVoices.ts
@@ -3,17 +3,37 @@ import { tRPCProcedure } from "@/server/tRPC";
 import { PRAOSessionRepositoryResultAsync } from "@/repository/PRAOSession";
 import { ObjectId } from "mongodb";
 import { TRPCError } from "@trpc/server";
-import { RedisClientResultAsync } from "@/lib/RedisClient";
+import { ResultAsync } from "neverthrow";
+import { ElevenLabsFreeSessionRepositoryResultAsync } from "@/repository/ElevenLabsFreeSessionRepository";
+import { ElevenLabsFreeSession } from "@/utils/prao/ElevenLabsFree/session";
+import { ElevenLabsFreeEngine } from "@/utils/prao/ElevenLabsFree/engine";
+import { sharedVoices } from "@/services/elevenLabsInternalAPI/sharedVoices";
+import { omit } from "lodash";
+import { getErrorMessage } from "@/utils/getErrorMessage";
 
 const InputSchema = z.object({
     praoSesssionID: z.hex().length(24).transform(ObjectId.createFromHexString),
     searchQuery: z.string().optional(),
-    sort: z.string().optional(),
-    pageSize: z.string().min(1).max(100).optional(),
+    sort: z.enum(["TRENDING"]).optional(),
+    pageSize: z.number().min(1).max(100).optional(),
     pageNum: z.int(),
 });
 
 export const sharedVoicesProcedure = tRPCProcedure.input(InputSchema).query(async ({ input: validatedInput }) => {
+    const result = await fn(validatedInput);
+
+    if (result.isErr()) {
+        throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            cause: result.error,
+            message: getErrorMessage(result.error),
+        });
+    }
+
+    return result.value;
+});
+
+function fn(input: z.output<typeof InputSchema>) {
     /* PRAOSessionRepositoryResultAsync.andThen((PRAOSessionRepository) =>
         PRAOSessionRepository.findOneByID(validatedInput.praoSesssionID),
     ).andThen((praoSession) => {
@@ -21,7 +41,17 @@ export const sharedVoicesProcedure = tRPCProcedure.input(InputSchema).query(asyn
 
         RedisClientResultAsync;
     }); */
-    const PRAOSessionRepositoryResultAsyncResult = await PRAOSessionRepositoryResultAsync;
+    // TODO: Complete this
+    /* ResultAsync.combine([PRAOSessionRepositoryResultAsync, RedisClientResultAsync ]).andThen(([PRAOSessionRepository, RedisClient]) => 
+        PRAOSessionRepository.findOneByID(input.praoSesssionID).andThen(praoSesssion => {
+            // praoSesssion.
+            RedisClient.get(`sessionID@${praoSesssion._id}`).andThen(parseJSON).andThen(PRAOSession.deserializeFromJSON).andThen(PRAOEngine.fromSession).andThen((praoEngine) => {
+                praoEngine.spend(0)
+            })
+        })
+    ) */
+    // const = await PRAOSessionRepositoryResultAsync;
+    /*
 
     if (PRAOSessionRepositoryResultAsyncResult.isErr()) {
         throw new TRPCError({
@@ -41,7 +71,7 @@ export const sharedVoicesProcedure = tRPCProcedure.input(InputSchema).query(asyn
         });
     }
 
-    const RedisClientResultAsyncResult = await RedisClientResultAsync;
+    const = await RedisClientResultAsync;
 
     if (RedisClientResultAsyncResult.isErr()) {
         throw new TRPCError({
@@ -56,4 +86,25 @@ export const sharedVoicesProcedure = tRPCProcedure.input(InputSchema).query(asyn
 
     if (redisSessionResult === null) {
     }
-});
+    */
+    return ResultAsync.combine([PRAOSessionRepositoryResultAsync, ElevenLabsFreeSessionRepositoryResultAsync]).andThen(
+        ([PRAOSessionRepository, ElevenLabsFreeSessionRepository]) =>
+            ElevenLabsFreeSessionRepository.get(input.praoSesssionID)
+                .andThen(ElevenLabsFreeSession.new)
+                .andThen((session) =>
+                    ElevenLabsFreeEngine.spend(session)
+                        .andThen((resource) => resource.getIDToken().map((idToken) => ({ resource, idToken })))
+                        .andThen(({ idToken, resource }) =>
+                            sharedVoices({
+                                bearerToken: idToken,
+                                proxyURL: resource.context.proxyURL,
+                                ...omit(input, "praoSession"),
+                            }).map((sharedVoices) => ({ sharedVoices, resource })),
+                        )
+                        .map(({ sharedVoices, resource }) => {
+                            resource.decrementBalance(1);
+                            return sharedVoices;
+                        }),
+                ),
+    );
+}
