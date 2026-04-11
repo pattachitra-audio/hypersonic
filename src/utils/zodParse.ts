1be491663e6f5c120c4eb385f64d0dd4ff647f39
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-11 14:18:56 +0530

    Added async zod parse

--- src/utils/zodParse.ts
+++ src/utils/zodParse.ts
@@ -1,4 +1,4 @@
-import { err, ok, Result } from "neverthrow";
+import { err, errAsync, ok, okAsync, Result, ResultAsync } from "neverthrow";
 import z from "zod";
 
 export function zodParse<Schema extends z.ZodType>(
@@ -12,4 +12,17 @@ export function zodParse<Schema extends z.ZodType>(
     }
 
     return err(result.error);
+}
+
+export function zodParseAsync<Schema extends z.ZodType>(
+    schema: Schema,
+    obj: unknown,
+): ResultAsync<z.infer<Schema>, z.ZodError<z.infer<Schema>>> {
+    return ResultAsync.fromSafePromise(schema.safeParseAsync(obj)).andThen((result) => {
+        if (result.success) {
+            return okAsync(result.data);
+        }
+
+        return errAsync(result.error);
+    });
 }
