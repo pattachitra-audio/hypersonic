70967002befc5d22726c0983b9c3874a0d5248e4
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-12 20:21:17 +0530

    Added create, satisfiability check, etc.

--- src/utils/prao/ElevenLabs/lanes/credit/roster.ts
+++ src/utils/prao/ElevenLabs/lanes/credit/roster.ts
@@ -1,18 +1,23 @@
 import z from "zod";
-import { ResultAsync } from "neverthrow";
+import { okAsync, ResultAsync } from "neverthrow";
 import { ElevenLabsCreditLaneEntry } from "./entry";
 import { zodParseAsync } from "@/utils/zodParse";
+import { JSONSerializable } from "@/interfaces/JSONSerializable";
+import { JSONDeserializable } from "@/interfaces/JSONDeserializable";
+import { Creator } from "@/interfaces/Creator";
 
 export class ElevenLabsCreditLaneRoster {
     public static Schema = z.object({
         entries: z.array(z.unknown()),
         totalBalance: z.number(),
+        totalBalanceStale: z.boolean(),
     });
 
     private constructor(
         public context: {
             entries: ElevenLabsCreditLaneEntry[];
             totalBalance: number;
+            totalBalanceStale: boolean;
         },
     ) {}
 
@@ -34,20 +39,39 @@ export class ElevenLabsCreditLaneRoster {
                     totalBalance,
                 })),
             )
-            .map((context) => new ElevenLabsCreditLaneRoster(context));
+            .map((context) => new ElevenLabsCreditLaneRoster({ ...context, totalBalanceStale: false }));
+    }
+
+    public static create(entries: ElevenLabsCreditLaneEntry[]) {
+        return ElevenLabsCreditLaneRoster.computeTotalBalance(entries).map(
+            (totalBalance) => new ElevenLabsCreditLaneRoster({ entries, totalBalance, totalBalanceStale: false }),
+        );
     }
 
     public get entries() {
         return this.context.entries;
     }
 
-    public computeTotalBalance() {
-        return ResultAsync.combine(this.context.entries.map((entry) => entry.balance)).map((balances) =>
+    public static computeTotalBalance(entries: ElevenLabsCreditLaneEntry[]) {
+        return ResultAsync.combine(entries.map((entry) => entry.balance)).map((balances) =>
             balances.reduce((a, b) => a + b, 0),
         );
     }
 
     public get totalBalance() {
-        return this.computeTotalBalance();
+        const self = this;
+
+        if (!self.context.totalBalanceStale) {
+            return okAsync(self.context.totalBalance);
+        }
+
+        return ElevenLabsCreditLaneRoster.computeTotalBalance(self.context.entries).map((totalBalance) => {
+            self.context.totalBalance = totalBalance;
+            return totalBalance;
+        });
     }
 }
+
+ElevenLabsCreditLaneRoster satisfies JSONSerializable<ElevenLabsCreditLaneRoster> &
+    JSONDeserializable<ElevenLabsCreditLaneRoster> &
+    Creator<ElevenLabsCreditLaneRoster>;
