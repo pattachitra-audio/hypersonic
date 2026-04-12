b95d59809d3cdf840a0ea47f857e432f0190616f
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-12 23:01:29 +0530

    sessionID to allocationID

--- src/repository/ElevenLabsCreditRosterRepository.ts
+++ src/repository/ElevenLabsCreditRosterRepository.ts
@@ -6,14 +6,14 @@ import { ok } from "neverthrow";
 
 export const ElevenLabsCreditRosterRepositoryResultAsync = RedisClientResultAsync.andThen((RedisClient) => {
     return ok({
-        set(sessionID: ObjectId, roster: ElevenLabsCreditLaneRoster) {
+        set(allocationID: ObjectId, roster: ElevenLabsCreditLaneRoster) {
             return ElevenLabsCreditLaneRoster.serializeToJSON(roster)
                 .map(JSON.stringify)
-                .andThen((s) => RedisClient.set(`ElevenLabsCreditLaneRoster@${sessionID}`, s));
+                .andThen((s) => RedisClient.set(`ElevenLabsCreditLaneRoster@${allocationID}`, s));
         },
 
-        get(sessionID: ObjectId) {
-            return RedisClient.get(`ElevenLabsCreditLaneRoster@${sessionID}`)
+        get(allocationID: ObjectId) {
+            return RedisClient.get(`ElevenLabsCreditLaneRoster@${allocationID}`)
                 .andThen(parseJSON)
                 .andThen(ElevenLabsCreditLaneRoster.deserializeFromJSON);
         },
