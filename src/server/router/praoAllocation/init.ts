4ea493bda15dbdb635c293ac19aa7a17c9716cd1
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-12 22:56:58 +0530

    Removed old commented code

--- src/server/router/praoAllocation/init.ts
+++ src/server/router/praoAllocation/init.ts
@@ -33,61 +33,6 @@ export const initProcedure = tRPCProcedure.input(InputSchema).mutation(async ({ 
     return result.value;
 });
 
-// const ElevenLabsAccountWithProxyRepositoryResult = await ElevenLabsAccountWithProxyRepositoryResultAsync;
-
-/* if (findAllByOwnerIDResult.isErr()) {
-        throw new TRPCError({
-            code: "NOT_FOUND",
-            message: `Failed to find all elevenLabs accounts for current ownerID`,
-        });
-    } */
-
-// const accounts = findAllByOwnerIDResult.value;
-
-/* const PRAOSessionRepositoryResult = await;
-
-    if (PRAOSessionRepositoryResult.isErr()) {
-        throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Failed to init 'PRAOSessionRepository'",
-            cause: PRAOSessionRepositoryResult.error,
-        });
-    }
-
-    const PRAOSessionRepository = PRAOSessionRepositoryResult.value; */
-
-/* const result = await resourcesSerializedResult;
-
-    if (result.isErr()) {
-        throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: getErrorMessage(result.error),
-            cause: result.error,
-        });
-    } */
-
-/* if (insertOneResult.isErr()) {
-        throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: getErrorMessage(insertOneResult.error),
-            cause: insertOneResult.error,
-        });
-    } */
-
-// const allocationID = insertOneResult.value.insertedId;
-
-/*
-    if (ElevenLabsAccountWithProxyRepositoryResult.isErr()) {
-        throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Failed to init 'ElevenLabsAccountWithProxyRepository'",
-            cause: ElevenLabsAccountWithProxyRepositoryResult.error,
-        });
-    }
-
-    const ElevenLabsAccountWithProxyRepository = ElevenLabsAccountWithProxyRepositoryResult.value;
-    */
-
 function filterUnlockedAccounts(accounts: WithId<ElevenLabsPoolDocumentType>[]) {
     for (const account of accounts) {
         if (account.allocationID) {
@@ -157,30 +102,3 @@ function initRedisAllocationCurry(allocationID: ObjectId, accounts: ElevenLabsPo
         );
     };
 }
-
-/* 
-                                .andThen(([creditsResources, freeResources]) =>
-                                    Result.combine([
-                                        Result.combine(creditsResources.map(ElevenLabsCreditsResource.serializeToJSON)),
-                                        Result.combine(freeResources.map(ElevenLabsFreeResource.serializeToJSON)),
-                                    ]),
-                                )
-                                .andThen(([creditsResourcesJSON, freeResourcesJSON]) => {
-                                    ResultAsync.combine([
-                                        RedisClient.set(
-                                            `ElevenLabsCreditsSession@${allocationID}`,
-                                            JSON.stringify(creditsResourcesJSON),
-                                        ),
-                                        RedisClient.set(
-                                            `ElevenLabsFreeSession@${allocationID}`,
-                                            JSON.stringify(freeResourcesJSON),
-                                        ),
-                                    ]);
-
-                                    return okAsync({
-                                        allocationID,
-                                        resources: creditsResourcesJSON.map((o) => pick(o, "id", "balance")),
-                                    });
-                                }), */
-
-// resources: creditsResourcesJSON.map((o) => pick(o, "id", "balance")),
