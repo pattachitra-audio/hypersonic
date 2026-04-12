8eb49f5922e6a964eddef9bdcbec76792f45e9da
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-12 23:17:57 +0530

    Added del method; Refactored key generation fn

--- src/repository/ElevenLabsCreditRosterRepository.ts
+++ src/repository/ElevenLabsCreditRosterRepository.ts
@@ -2,20 +2,28 @@ import { RedisClientResultAsync } from "@/lib/RedisClient";
 import { parseJSON } from "@/utils/parseJSON";
 import { ElevenLabsCreditLaneRoster } from "@/utils/prao/ElevenLabs/lanes/credit/roster";
 import { ObjectId } from "mongodb";
-import { ok } from "neverthrow";
+import { okAsync } from "neverthrow";
+
+function generateKey(id: ObjectId) {
+    return `ElevenLabsCreditLaneRoster@${id}`;
+}
 
 export const ElevenLabsCreditRosterRepositoryResultAsync = RedisClientResultAsync.andThen((RedisClient) => {
-    return ok({
+    return okAsync({
         set(allocationID: ObjectId, roster: ElevenLabsCreditLaneRoster) {
             return ElevenLabsCreditLaneRoster.serializeToJSON(roster)
                 .map(JSON.stringify)
-                .andThen((s) => RedisClient.set(`ElevenLabsCreditLaneRoster@${allocationID}`, s));
+                .andThen((s) => RedisClient.set(generateKey(allocationID), s));
         },
 
         get(allocationID: ObjectId) {
-            return RedisClient.get(`ElevenLabsCreditLaneRoster@${allocationID}`)
+            return RedisClient.get(generateKey(allocationID))
                 .andThen(parseJSON)
                 .andThen(ElevenLabsCreditLaneRoster.deserializeFromJSON);
+        },
+
+        del(allocationID: ObjectId) {
+            return RedisClient.del(generateKey(allocationID));
         },
     });
 });
--- src/repository/ElevenLabsFreeRosterRepository.ts
+++ src/repository/ElevenLabsFreeRosterRepository.ts
@@ -4,18 +4,26 @@ import { ElevenLabsFreeLaneRoster } from "@/utils/prao/ElevenLabs/lanes/free/ros
 import { ObjectId } from "mongodb";
 import { ok } from "neverthrow";
 
+function generateKey(id: ObjectId) {
+    return `ElevenLabsFreeLaneRoster@${id}`;
+}
+
 export const ElevenLabsFreeRosterRepositoryResultAsync = RedisClientResultAsync.andThen((RedisClient) => {
     return ok({
         set(allocationID: ObjectId, roster: ElevenLabsFreeLaneRoster) {
             return ElevenLabsFreeLaneRoster.serializeToJSON(roster)
                 .map(JSON.stringify)
-                .andThen((s) => RedisClient.set(`ElevenLabsFreeLaneRoster@${allocationID}`, s));
+                .andThen((s) => RedisClient.set(generateKey(allocationID), s));
         },
 
         get(allocationID: ObjectId) {
-            return RedisClient.get(`ElevenLabsFreeLaneRoster@${allocationID}`)
+            return RedisClient.get(generateKey(allocationID))
                 .andThen(parseJSON)
                 .andThen(ElevenLabsFreeLaneRoster.deserializeFromJSON);
+        },
+
+        del(allocationID: ObjectId) {
+            return RedisClient.del(generateKey(allocationID));
         },
     });
 });
