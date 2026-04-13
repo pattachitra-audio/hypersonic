78547050b787a2544bb004fe325a2e8297663f43
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-04-13 18:36:55 +0530

    Updated the AudioBook project create method

--- src/server/router/project/create.ts
+++ src/server/router/project/create.ts
@@ -4,30 +4,35 @@ import { tRPCProcedure } from "@/server/tRPC";
 import { AudioBookRepositoryResultAsync } from "@/repository/AudioBookRepository";
 import { TRPCError } from "@trpc/server";
 import { ObjectId } from "mongodb";
+import { PRAOAllocationRepositoryResultAsync } from "@/repository/PRAOAllocation";
+import { ResultAsync } from "neverthrow";
+import { getErrorMessage } from "@/utils/getErrorMessage";
 
 const InputSchema = AudioBookSchema.extend({
     allocationID: z.hex().length(24),
 });
 
 export const createProcedure = tRPCProcedure.input(InputSchema).mutation(async ({ input }) => {
-    const AudioBookRepositoryResult = await AudioBookRepositoryResultAsync;
+    const result = await fn(input);
 
+    if (result.isErr()) {
+        throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: getErrorMessage(result.error),
+            cause: result.error,
+        });
+    }
+
+    return result.value;
+});
+
+/*
     if (AudioBookRepositoryResult.isErr()) {
         throw new TRPCError({
             code: "INTERNAL_SERVER_ERROR",
             message: "Failed to init 'AudioBookRepository'",
         });
     }
-
-    const AudioBookRepository = AudioBookRepositoryResult.value;
-    const insertOneResult = await AudioBookRepository.insertOne({
-        ...input,
-        allocationID: ObjectId.createFromHexString(input.allocationID),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        lastAccessedAt: new Date(),
-        status: "ACTIVE",
-    });
 
     if (insertOneResult.isErr()) {
         throw new TRPCError({
@@ -37,5 +42,23 @@ export const createProcedure = tRPCProcedure.input(InputSchema).mutation(async (
         });
     }
 
-    return insertOneResult.value.insertedId.toString("hex");
-});
+*/
+
+function fn(input: z.output<typeof InputSchema>) {
+    return ResultAsync.combine([AudioBookRepositoryResultAsync, PRAOAllocationRepositoryResultAsync])
+        .andThen(([AudioBookRepository, PRAOAllocationRepository]) => {
+            const allocationID = ObjectId.createFromHexString(input.allocationID);
+
+            return PRAOAllocationRepository.findOneByID(allocationID).andThen(() => {
+                return AudioBookRepository.insertOne({
+                    ...input,
+                    allocationID,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    lastAccessedAt: new Date(),
+                    status: "ACTIVE",
+                });
+            });
+        })
+        .map(({ insertedId }) => insertedId.toString("hex"));
+}
