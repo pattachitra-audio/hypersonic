3c36e4aa25244a9812231f2d8c79730efebec6e2
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-05-02 11:53:50 +0530

    Added procedure to router

--- src/server/router/elevenLabsInternal/index.ts
+++ src/server/router/elevenLabsInternal/index.ts
@@ -1,6 +1,8 @@
 import { tRPCRouter } from "@/server/tRPC";
 import { sharedVoicesProcedure } from "./sharedVoices";
+import { enhanceDialogueProcedure } from "./enhanceDialogue";
 
 export const elevenLabsInternalRouter = tRPCRouter({
     sharedVoices: sharedVoicesProcedure,
+    enhanceDialogue: enhanceDialogueProcedure,
 });
