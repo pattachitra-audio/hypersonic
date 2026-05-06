7148bf9744d950da7e4f6d7237a8a6cc7589927e
Pattachitra Audio <pattachitra.audio@gmail.com>
2026-05-06 21:41:58 +0530

    Removed constants URL

--- src/services/elevenLabsFirebase/exchangeRefreshTokenForIDToken/index.ts
+++ src/services/elevenLabsFirebase/exchangeRefreshTokenForIDToken/index.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 import { requestHeaders } from "@/requestHeaders";
-import { ELEVEN_LABS_FIREBASE_API_KEY, FIREBASE_BASE_URL } from "../constants";
+import { ELEVEN_LABS_FIREBASE_API_KEY } from "../constants";
 import { InputSchema } from "./input";
 import { OutputSchema } from "./output";
 import { undiciFetch } from "@/utils/undiciFetch";
@@ -16,7 +16,7 @@ function fn(validatedInput: z.output<typeof InputSchema>) {
     data.append("grant_type", "refresh_token");
     data.append("refresh_token", validatedInput.refreshToken);
 
-    const url = `${FIREBASE_BASE_URL}/v1/token?key=${ELEVEN_LABS_FIREBASE_API_KEY}`;
+    const url = `https://securetoken.googleapis.com/v1/token?key=${ELEVEN_LABS_FIREBASE_API_KEY}`;
 
     return undiciFetch(url, {
         method: "POST",
